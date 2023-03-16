b3d140752a848a979e929ae6731d44271b274411
CodeMarshal007 <princeaayusuf@gmail.com>
2023-03-16 07:10:12 -0700

    fix betty doc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-int main(void) { printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", (int)sizeof(char), (int)sizeof(int), (int)sizeof(long int), (int)sizeof(long long int), (int)sizeof(float)); return 0;}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", (int)sizeof(char), (int)sizeof(int), (int)sizeof(long int), (int)sizeof(long long int), (int)sizeof(float));
+	return (0);
+}
