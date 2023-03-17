4ac660c983ef2b698c80163ec594ce557d2586c9
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-03-17 21:08:29 +0100

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: 'this program print on'
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
