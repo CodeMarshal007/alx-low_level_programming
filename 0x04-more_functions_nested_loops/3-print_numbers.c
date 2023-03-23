7cbb88fb0284100bd26bac8a11060605bbbd5747
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-03-23 14:54:20 +0100

    prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int print_numbers - prints the numbers, from 0 to 9.
+ *
+ * Return:
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
