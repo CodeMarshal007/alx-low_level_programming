2a23ceaeb10384de1439e77383aef6a9ee31063d
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-03-23 14:22:08 +0100

    checks for a digit (0 through 9)

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * DESC: Header file containing declarations for all functions
+ * used in the 0x04 directory
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+
+#endif
