e3b2245ca14b2d112c0a9865c19e9a6356374082
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-03-27 21:41:39 +0100

    create main.h header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * DESC: Header file containing declarations for all functions
+ * used in the 0x05 directory
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
+
