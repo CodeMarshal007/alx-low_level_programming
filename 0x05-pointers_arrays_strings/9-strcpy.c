becd70efc60df7e7665c1f216403f98c8be61d19
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-03-28 09:04:37 +0100

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy
+ -    prints n elements of an array of integers
+ * @src: pointing to source string
+ * @dest: pointing to destination string
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, len = 0;
+
+	while (src[len] != '\n')
+		len++;
+
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
