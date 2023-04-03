c1747992cc7f6736dde2fa89316eef154e6e4458
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-03 21:51:37 +0100

    fills

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * DESC: Header file containing declarations for all functions
+ * used in the 0x07 directory
+ */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
+
