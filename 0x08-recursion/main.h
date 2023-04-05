07bdad21b60e1c9fd53c8a5f0dde916e9fc8c773
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-05 17:55:35 +0100

    prints a string, followed by a new line

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * DESC: Header file containing declarations for all functions
+ * used in the 0x08 directory
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
