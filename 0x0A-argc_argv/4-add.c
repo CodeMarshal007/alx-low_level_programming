c0786d3744f524b94ae1d809ffdb5aede9957f1a
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-07 18:05:35 +0100

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < 48 || *argv[i] > 57)
+		if (*argv[i] != '-' && (*argv[i] < '0' || *argv[i] > '9'))
 		{
 			puts("Error");
 
