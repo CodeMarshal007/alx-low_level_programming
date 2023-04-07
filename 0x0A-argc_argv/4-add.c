05a229deb247c23e40d5337d350077e00a85f891
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-07 17:50:40 +0100

    a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description:  a program that adds positive numbers.
+ * @argc: number of arguments passed to the program.
+ * @argv: array of arguments that was passed to the program.
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0, i = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < 48 || *argv[i] > 57)
+		{
+			puts("Error");
+
+			putchar('\n');
+
+			return (1);
+		}
+		else
+
+			result = result + atoi(argv[i]);
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
