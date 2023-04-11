440ac40e26f60e782a61db77bddf3dcead253300
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-11 10:47:09 +0100

    fix Betty coding style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,43 +14,26 @@ char *str_concat(char *s1, char *s2)
 	unsigned int length1 = 0, length2 = 0, totalLength = 0;
 	unsigned int i = 0, j = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[length1] != '\0')
-		length1++;
-
-	while (s2[length2] != '\0')
-		length2++;
-
+	if (s1 == NULL) s1 = "";
+	if (s2 == NULL) s2 = "";
+	while (s1[length1] != '\0') length1++;
+	while (s2[length2] != '\0') length2++;
 	totalLength = length1 + length2 + 1;
 
 	ptr = (char *)malloc(totalLength);
 
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0; i <= totalLength; i++)
+	if (ptr == NULL) return (NULL);
+	while (s1[i] != '\0')
 	{
-		while (s1[i] != '\0')
-		{
-			ptr[i] = s1[i];
-
-			i++;
-		}
-
-		while (s2[j] != '\0')
-		{
-			ptr[i] = s2[j];
-
-			i++;
-			j++;
-		}
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
 	}
 	ptr[i] = '\0';
-
 	return (ptr);
 }
