5460c41ab3cdc5d76162bf9349dc01bcbfdbaae7
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-11 10:56:21 +0100

    fix Betty coding style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,15 +13,16 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	unsigned int length1 = 0, length2 = 0, totalLength = 0;
 	unsigned int i = 0, j = 0;
-
-	if (s1 == NULL) s1 = "";
-	if (s2 == NULL) s2 = "";
-	while (s1[length1] != '\0') length1++;
-	while (s2[length2] != '\0') length2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[length1] != '\0')
+		length1++;
+	while (s2[length2] != '\0')
+		length2++;
 	totalLength = length1 + length2 + 1;
-
 	ptr = (char *)malloc(totalLength);
-
 	if (ptr == NULL) return (NULL);
 	while (s1[i] != '\0')
 	{
