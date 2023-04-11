a72ca3dc0090be1ab3f561a73f28daae97ef6409
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-11 10:58:52 +0100

    fix Betty coding style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,18 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * str_concat -   a function that concatenates two strings.
  * @s1: string one to concatenate
  * @s2: string one to concatenate
  * Return: pointer to concat string or NULL on failure
  */
-
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
 	unsigned int length1 = 0, length2 = 0, totalLength = 0;
 	unsigned int i = 0, j = 0;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -23,7 +22,8 @@ char *str_concat(char *s1, char *s2)
 		length2++;
 	totalLength = length1 + length2 + 1;
 	ptr = (char *)malloc(totalLength);
-	if (ptr == NULL) return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 	while (s1[i] != '\0')
 	{
 		ptr[i] = s1[i];
