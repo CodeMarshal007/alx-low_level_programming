68dde39ff4ee53e8c3cff080a1c480bb326edb0c
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-13 17:26:15 +0100

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * DESC: Header file containing declarations for all functions
+ * used in the 0x0C-more_malloc_free directory
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
