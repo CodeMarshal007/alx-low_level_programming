e40a3ac7b105b957775434fe2a9c24586901df30
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-17 22:27:08 +0100

    define a macro named SIZE.

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,8 +2,6 @@
 #define OBJECT_LIKE_MACRO_H
 
 /**
- * Description:  Header file containing declarations of function
- * and macros used in the 0x0D-preprocessor directory
  * SIZE - a macro with token of 1024
  */
 
