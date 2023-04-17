a87ac71c537d816e5ed41a0dca0bfb8a8f648979
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-17 12:10:53 +0100

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -  definition for a new struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description:  Header file containing definition for a new struct dog
+ *  used in the 0x0E-structures_typedef directory
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
