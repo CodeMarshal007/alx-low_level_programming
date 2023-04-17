a65edbedd6772d7be1df85ffcc575697f719216d
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-17 16:28:09 +0100

    edit documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,14 +4,15 @@
 /**
  * Description:  Header file containing declarations function
  *  used in the 0x0E-structures_typedef directory
- */
-
-/**
+ *
  * struct dog -  definition for a new struct dog
  * @name: name
  * @age: age
  * @owner: owner
+ *
+ * dog_t - Typedef for struct dog
  */
+
 struct dog
 {
     char *name;
@@ -21,10 +22,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-/**
- * dog_t - Typedef for struct dog
- */
 
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
