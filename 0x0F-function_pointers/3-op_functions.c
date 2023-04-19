1f915c8892faa1824bfd9d4fbf04f8cffddb9f8c
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-19 21:16:40 +0100

    add function description

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -17,23 +17,35 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
-
+/**
+ * op_sub - searches for an integer.
+ * Return: computation result
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
+/**
+ * op_mul - searches for an integer.
+ * Return: computation result
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
+/**
+ * op_div - searches for an integer.
+ * Return: computation result
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
+/**
+ * op_mod - searches for an integer.
+ * Return: computation result
+ */
 int op_mod(int a, int b)
 {
-    return (a % b);
+	return (a % b);
 }
