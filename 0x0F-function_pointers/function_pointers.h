6deb879f79e14d5a565037cda355c4cc61577e5c
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-19 11:06:24 +0100

    executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
 
+#include <stddef.h>
+
 /**
  * DESC: Header file containing declarations for all functions
  * used in the 0x0F-function_pointers directory
@@ -8,5 +10,6 @@
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
