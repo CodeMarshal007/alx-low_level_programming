36382b2f5919a2bd91e5a8a9faecf44dfedf6c63
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-19 10:27:17 +0100

    a function that prints a name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
+/**
+ * DESC: Header file containing declarations for all functions
+ * used in the 0x0F-function_pointers directory
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
