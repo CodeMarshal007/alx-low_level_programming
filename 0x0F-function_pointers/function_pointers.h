8ec606a45ef54bca62b6f1a833139836a6fcec29
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-19 17:43:18 +0100

    fix betty coding style

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,9 +7,8 @@
  * DESC: Header file containing declarations for all functions
  * used in the 0x0F-function_pointers directory
  */
-
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
