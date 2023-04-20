80d209c4f1d981f2a803e8badb22ede4be36e03e
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-20 15:38:42 +0100

    fix betty coding style

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,48 +9,40 @@
  */
 void print_all(const char *const format, ...)
 {
-    unsigned int i = 0, j;
-    char c, *s;
-    int n;
-    float f;
+	unsigned int i = 0, j;
+	char *s;
+	va_list list_params;
 
-    va_list list_params;
-
-    va_start(list_params, format);
-
-    while (format[i] != '\0')
-    {
-        switch (format[i])
-        {
-        case 'c':
-            c = va_arg(list_params, int);
-            printf("%c", c);
-            break;
-        case 'i':
-            n = va_arg(list_params, int);
-            printf("%d", n);
-            break;
-        case 'f':
-            f = va_arg(list_params, double);
-            printf("%f", f);
-            break;
-        case 's':
-            s = va_arg(list_params, char *);
-            if (s == NULL)
-                printf("(nil)");
-            else
-                printf("%s", s);
-            break;
-        default:
-            i++;
-            continue;
-        }
-
-        j = i + 1;
-        if (format[j] != '\0')
-            printf(", ");
-        i++;
-    }
-    printf("\n");
-    va_end(list_params);
+	va_start(list_params, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(list_params, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(list_params, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(list_params, double));
+			break;
+		case 's':
+			s = va_arg(list_params, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+			break;
+		default:
+			i++;
+			continue;
+		}
+		j = i + 1;
+		if (format[j] != '\0')
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(list_params);
 }
