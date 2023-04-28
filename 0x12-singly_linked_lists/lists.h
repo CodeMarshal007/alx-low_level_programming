d841ba92a360f0f3aa7e3486bd2ecaa9d1cd3c76
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-29 00:30:13 +0100

    fix betty documentation

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,9 +27,15 @@ int _putchar(char c);
 /**
  * print_list - prints all the elements of a list_t list
  * @h: head
- * Return : number of nodes
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h);
+
+/**
+ * list_len - prints all the elements of a list_t list
+ * @h: head
+ * Return: number of nodes
+ */
 size_t list_len(const list_t *h);
 
 #endif
