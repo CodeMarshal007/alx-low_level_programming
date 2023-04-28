140b8de5b603109a55c822a62c993ef0a1d0080b
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-28 23:55:50 +0100

    prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head
+ * Return : number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current;
+	size_t count = 0;
+
+	if (h == NULL)
+		exit(98);
+
+	current = h;
+
+	while (current != NULL)
+	{
+	printf("[%d] %s\n", current->len, current->str);
+	count++;
+	current = current->next;
+	}
+
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,34 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
+ * _putchar - write a char to the std output
+ * @c: char
+ * Return : length of char written
+ */
+int _putchar(char c);
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head
+ * Return : number of nodes
+ */
+size_t print_list(const list_t *h);
+
+#endif
