9c122e04ac53d8a84fa878227605b903c9533bcb
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-29 00:24:07 +0100

    fix a typo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -21,7 +21,8 @@ size_t print_list(const list_t *h)
 
 	while (current != NULL)
 	{
-	printf("[%d] %s\n", current->len, (current->str) == NULL ? "(nil)" : current->str));
+	printf("[%d] %s\n", current->len,
+			(current->str) == NULL ? "(nil)" : current->str);
 	count++;
 	current = current->next;
 	}
