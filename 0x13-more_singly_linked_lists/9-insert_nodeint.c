134abccda5c020558de002be79fa69826d0b7989
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-04-30 18:01:48 +0100

    inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,6 +41,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prev_node = current_node;
 		current_node = current_node->next;
 	}
-	free(new_node);
+	
 	return (NULL);
 }
