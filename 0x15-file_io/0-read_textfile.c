f6a70536d095e312e3d73e2e9b5f71f8ad96dc29
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-05-25 13:50:36 +0100

    correct output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: file
+ * @filename: file to read from
  * @letters: number of letters it should read and print
  *
- * Return: number of letters it could read and print else 0 if failed
+ * Return: number of letters it could read and print, or 0 if failed
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, bytes_read;
 	char buffer[BUFFER_SIZE];
 	ssize_t total_bytes_read = 0;
 
-	if (filename == NULL || letters == 0)
+	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) >
-			0 && total_bytes_read < (ssize_t)letters)
+	while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0
+	&& total_bytes_read < (ssize_t)letters)
 	{
 		if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
 		{
@@ -35,11 +35,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		if (total_bytes_read >= (ssize_t)letters)
 			break;
 	}
-	if (bytes_read == -1)
-	{
-		close(fd);
-		return (0);
-	}
+
 	close(fd);
 	return (total_bytes_read);
 }
