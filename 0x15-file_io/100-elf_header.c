b84a74003977fb2638253aa3621ba045bc8c805d
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2023-05-25 14:23:22 +0100

    correct output

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -176,37 +176,29 @@ void print_header(Elf64_Ehdr *header)
  */
 int main(int argc, char *argv[])
 {
-    int fd;
-    Elf64_Ehdr header;
-
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        exit(98);
-    }
-    fd = open(argv[1], O_RDONLY);
-    if (fd < 0)
-    {
-        fprintf(stderr, "Error: could not open file '%s'\n", argv[1]);
-        exit(98);
+    if (argc != 2) {
+        printf("Usage: %s <elf_file>\n", argv[0]);
+        return 1;
     }
 
-    if (read(fd, &header, sizeof(header)) < 0)
-    {
-        fprintf(stderr, "Error: could not read ELF header from file '%s'\n", argv[1]);
-        exit(98);
+ 
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return 1;
     }
 
-    if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
-    {
-        fprintf(stderr, "Error: file '%s' is not an ELF file\n", argv[1]);
-        exit(98);
+     Elf64_Ehdr header;
+    ssize_t bytes_read = read(fd, &header, sizeof(header));
+    if (bytes_read != sizeof(header)) {
+        perror("read");
+        close(fd);
+        return 1;
     }
 
-    print_header(&header);
+     print_header(&header);
 
-    close(fd);
+     close(fd);
 
     return 0;
 }
